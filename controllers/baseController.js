c672c16812b71c6ee894fd8a4afdd24423935d87
clarnx <clarenceonline@outlook.com>
2025-11-27 15:25:49 +0000

    added session and account controller and route.

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -3,11 +3,12 @@ const baseController = {};
 
 baseController.buildHome = async function (req, res) {
   const nav = await utilities.getNav();
+
   res.render("index", { title: "Home", nav });
 };
 
 baseController.triggerError = async function (req, res, next) {
-  const error = new Error('Intentional 500 error triggered');
+  const error = new Error("Intentional 500 error triggered");
   error.status = 500;
   next(error);
 };
