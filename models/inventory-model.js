c058cec2d293724a9535fd4331bf9e9af2d1224a
clarnx <clarenceonline@outlook.com>
2025-12-04 23:24:08 +0000

    added delete functionality

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -124,11 +124,25 @@ async function updateInventory(
   }
 }
 
+/* ***************************
+ *  Delete Inventory Item
+ * ************************** */
+async function deleteInventoryItem(inv_id) {
+  try {
+    const sql = 'DELETE FROM public.inventory WHERE inv_id = $1';
+    const data = await pool.query(sql, [inv_id]);
+    return data;
+  } catch (error) {
+    return new Error("Delete Inventory Error");
+  }
+}
+
 module.exports = {
   getClassifications,
   getInventoryByClassificationId,
   getVehicleByInventoryId,
   addClassification,
   addInventory,
-  updateInventory
+  updateInventory,
+  deleteInventoryItem
 };
