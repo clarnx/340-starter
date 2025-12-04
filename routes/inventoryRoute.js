6dedb1900c04f06dcd55631873348c06949cbdb2
A.E Clarence <33183127+clarnx@users.noreply.github.com>
2025-12-04 23:00:11 +0000

    Updated invController.js

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -39,4 +39,7 @@ router.post(
 // Route to get inventory by classification as JSON
 router.get("/getInventory/:classification_id", utilities.handleErrors(invController.getInventoryJSON));
 
+// Route to build edit inventory view
+router.get("/edit/:inv_id", utilities.handleErrors(invController.editInventoryView));
+
 module.exports = router;
